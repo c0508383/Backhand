b59956487995bfbf379d142d77e1a557e941e6bf
c0508383 <luiscardenas826@yahoo.com>
2022-08-09 21:21:08 -0500

    Backlisted items can no loger be dragged into the offhand slot.

--- src/main/java/mods/battlegear2/api/core/ContainerPlayerBattle.java
+++ src/main/java/mods/battlegear2/api/core/ContainerPlayerBattle.java
@@ -2,10 +2,8 @@ package mods.battlegear2.api.core;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.ContainerPlayer;
-import net.minecraft.inventory.InventoryCrafting;
-import net.minecraft.inventory.Slot;
-import net.minecraft.inventory.SlotCrafting;
+import net.minecraft.inventory.*;
+import net.minecraft.item.ItemStack;
 import xonin.backhand.Backhand;
 
 public class ContainerPlayerBattle extends ContainerPlayer {
@@ -23,7 +21,19 @@ public class ContainerPlayerBattle extends ContainerPlayer {
                     slot.yDisplayPosition = 21 + ((slot.slotNumber/2)%2) * 18;
                 }
             }
-            this.addSlotToContainer(new Slot(p_i1819_1_, InventoryPlayerBattle.OFFHAND_ITEM_INDEX, 80, 62));
+            this.addSlotToContainer(new OffhandSlot(p_i1819_1_, InventoryPlayerBattle.OFFHAND_ITEM_INDEX, 80, 62));
+        }
+    }
+
+    public static class OffhandSlot extends Slot
+    {
+        public OffhandSlot(IInventory p_i1824_1_, int p_i1824_2_, int p_i1824_3_, int p_i1824_4_) {
+            super(p_i1824_1_, p_i1824_2_, p_i1824_3_, p_i1824_4_);
+        }
+
+        public boolean isItemValid(ItemStack p_75214_1_)
+        {
+            return !Backhand.isOffhandBlacklisted(p_75214_1_);
         }
     }
 }
